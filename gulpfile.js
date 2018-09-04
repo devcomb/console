7a51352d5aa8fd5b4ff579c180e1ed535ef9b23d
James Drummond <james@devcomb.com>
2018-09-03 20:00:36 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,20 @@ var nodemon = require('gulp-nodemon');
 var sass        = require('gulp-sass');
 
 
-gulp.task('default', ['browser-sync'], function () {
+gulp.task('default', ['browser-sync-test'], function () {
+});
+
+gulp.task('browser-sync-test', ['sass'], function() {
+	browserSync.init(null, {
+	//proxy: "http://localhost:5000",
+        //files: ["app/*.*"],
+	open: false,
+	//host: process.env.HOSTNAME,
+	server: "./app",
+        port: 8080,
+	});
+    	gulp.watch("app/scss/*.scss", ['sass']);
+    	gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('browser-sync', ['nodemon'], function() {
