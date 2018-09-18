4b51cdb3eb8246f661c26b43e340e90867347630
james@devcomb.com <james@devcomb.com>
2018-09-18 22:32:37 +0000

    fixup

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,34 @@ var sass        = require('gulp-sass');
 const templateSet = require('swagger-template-es6-server');
 const codegen = require('gulp-swagger-codegen');
 
-gulp.task('default', ['browser-sync'], function () {
-});
+gulp.task('startNodemon',  function(done) {
+  const STARTUP_TIMEOUT = 5000;
+  const server = nodemon({
+    script: 'app.js',
+    stdout: false // without this line the stdout event won't fire
+  });
+  let starting = false;
 
-gulp.task('browser-sync', ['generate-code','sass'], function() {
-	browserSync.init({
-	// proxy: "http://localhost:5000",
-    //     files: ["app/*.*"],
-	open: false,
-	host: process.env.HOSTNAME,
-    notify: false,
-    localOnly: true,
-	server: "./app",
-        port: 8080,
-	});
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+  const onReady = () => {
+    starting = false;
+    done();
+  };
+
+  server.on('start', () => {
+    starting = true;
+    setTimeout(onReady, STARTUP_TIMEOUT);
+  });
+
+  server.on('stdout', (stdout) => {
+    process.stdout.write(stdout); // pass the stdout through
+    if (starting) {
+      onReady();
+    }
+  });
 });
 
 gulp.task('sass',  function() {
-    return gulp.src("app/scss/*.scss")
+    gulp.src("app/scss/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
@@ -38,3 +46,23 @@ gulp.task('generate-code', () =>
       implementationPath: '../implementation',
     })))
     .pipe(gulp.dest('./examples')));
+
+gulp.task('browser-sync', function(done) {
+	browserSync.init({
+	// proxy: "http://localhost:5000",
+    //     files: ["app/*.*"],
+	open: false,
+	host: process.env.HOSTNAME,
+    notify: false,
+    localOnly: true,
+	server: "./app",
+        port: 8080,
+	},done);
+    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series('startNodemon','browser-sync', 'generate-code','sass') );
+
+
+
