f4ede30cf4e2f9b17bb042f4c7a8d07931e8faf1
devcomb <james@devcomb.com>
2018-09-25 23:50:04 +0000

    fixup

---
+++ api/impl_pets.js
@@ -0,0 +1,89 @@
+/**
+ * @param {Object} options
+ * @param {} options.limit How many items to return at one time (max 100)
+ * @throws {Error}
+ * @return {Promise}
+ */
+module.exports.listPets = async (options) => {
+  // Implement your business logic here...
+  //
+  // This function should return as follows:
+  //
+  // return {
+  //   status: 200, // Or another success code.
+  //   data: [] // Optional. You can put whatever you want here.
+  // };
+  //
+  // If an error happens during your business logic implementation,
+  // you should throw an error as follows:
+  //
+  // throw new Error({
+  //   status: 500, // Or another error code.
+  //   error: 'Server Error' // Or another error message.
+  // });
+
+  return {
+    status: 200,
+    data: 'listPets ok!'
+  };
+};
+
+/**
+ * @param {Object} options
+ * @throws {Error}
+ * @return {Promise}
+ */
+module.exports.createPets = async (options) => {
+  // Implement your business logic here...
+  //
+  // This function should return as follows:
+  //
+  // return {
+  //   status: 200, // Or another success code.
+  //   data: [] // Optional. You can put whatever you want here.
+  // };
+  //
+  // If an error happens during your business logic implementation,
+  // you should throw an error as follows:
+  //
+  // throw new Error({
+  //   status: 500, // Or another error code.
+  //   error: 'Server Error' // Or another error message.
+  // });
+
+  return {
+    status: 200,
+    data: 'createPets ok!'
+  };
+};
+
+/**
+ * @param {Object} options
+ * @param {} options.petId The id of the pet to retrieve
+ * @throws {Error}
+ * @return {Promise}
+ */
+module.exports.showPetById = async (options) => {
+  // Implement your business logic here...
+  //
+  // This function should return as follows:
+  //
+  // return {
+  //   status: 200, // Or another success code.
+  //   data: [] // Optional. You can put whatever you want here.
+  // };
+  //
+  // If an error happens during your business logic implementation,
+  // you should throw an error as follows:
+  //
+  // throw new Error({
+  //   status: 500, // Or another error code.
+  //   error: 'Server Error' // Or another error message.
+  // });
+
+  return {
+    status: 200,
+    data: 'showPetById ok!'
+  };
+};
+
--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 
 // simple express server
 var express = require('express'); 
-var app = express();
-var router = express.Router();
+var api = require('./api/gen/src/api/index.js'); 
+// var app = express();
+// var router = express.Router();
 
-app.use(express.static('app'));
+// app.use(express.static('app'));
 
-app.get('/', function(req, res) {
-    res.sendfile('./app/index.html');
-});
+// app.get('/', function(req, res) {
+//     res.sendfile('./app/index.html');
+// });
+
+
 
 
 var appPort = Object.is(process.env.APP_PORT, undefined) ? 8080 : process.env.APP_PORT;
