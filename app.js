f30dda0e170c4258208766f868e94cf3bc4f4cc6
devcomb <james@devcomb.com>
2018-09-25 14:37:42 +0000

    fixup

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const wafflesImplementation = require('./examples/controllers/waffles');
-
 // simple express server
 var express = require('express'); 
 var app = express();
@@ -12,11 +10,7 @@ app.use(express.static('app'));
 app.get('/', function(req, res) {
     res.sendfile('./app/index.html');
 });
-console.log("test1");
 
-app.get('/waffles/', wafflesImplementation.getWaffleList);
-
-console.log("test2");
 
 var appPort = Object.is(process.env.APP_PORT, undefined) ? 8080 : process.env.APP_PORT;
 
