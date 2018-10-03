3b32b315fd78907361f8f66d94733e15209cc43b
devcomb <james@devcomb.com>
2018-10-03 00:26:17 +0000

    fixup

--- app.js
+++ app.js
@@ -6,14 +6,24 @@ var app = require('./api/gen/src/api/index.js');
 // var app = express();
 // var router = express.Router();
 
-// app.use(express.static('app'));
+app.use('/',express.static('app'));
 
-// app.get('/', function(req, res) {
-//     res.sendfile('./app/index.html');
-// });
+ app.get('/', function(req, res) {
+    res.sendfile('./app/index.html');
+ });
 
+ // catch 404
+app.use((req, res, next) => {
+  console.log(`Error 404 on ${req.url}.`);
+  res.status(404).send({ status: 404, error: 'Not found' });
+});
 
-
+// catch errors
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  console.log(`Error ${status} (${err.message}) on ${req.method} ${req.url} with payload ${req.body}.`);
+  res.status(status).send({ status, error: 'Server error' });
+});
 
 var appPort = Object.is(process.env.APP_PORT, undefined) ? 8080 : process.env.APP_PORT;
 
--- templates/express/src/api/index.js
+++ templates/express/src/api/index.js
@@ -22,17 +22,4 @@ app.use('{{@root.openapi.basePath}}/{{..}}', require('./routes/{{..}}'));
 {{/endsWith}}
 {{/each}}
 
-// catch 404
-app.use((req, res, next) => {
-  log.error(`Error 404 on ${req.url}.`);
-  res.status(404).send({ status: 404, error: 'Not found' });
-});
-
-// catch errors
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  log.error(`Error ${status} (${err.message}) on ${req.method} ${req.url} with payload ${req.body}.`);
-  res.status(status).send({ status, error: 'Server error' });
-});
-
 module.exports = app;
