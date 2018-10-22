d8b531842cac97353496cbb7ad7823db8f376d3a
devcomb <james@devcomb.com>
2018-10-22 20:38:26 +0000

    fixup

--- app/components/test.mjs
+++ app/components/test.mjs
@@ -1,16 +1,81 @@
-import Multipane from './vue-multipane.mjs';
+import {Multipane,MultipaneResizer} from '../dist/vue-multipane.esm.js';
 export default Vue.component('test', {
   template: `
-  <multipane v-on:paneResizeStop=paneResizeStop v-on:paneResizeStart=paneResizeStart v-on:paneResize=paneResize class="vertical-panes flex "   layout="vertical">
+<multipane v-on:paneResizeStop=paneResizeStop v-on:paneResizeStart=paneResizeStart v-on:paneResize=paneResize layout="vertical" class="multipane layout-v" v-bind:style="[styleObject1]" >
+   
+    <div  v-bind:style="[stylePane,stylePaneLeft]" class="pane mr-1 w-1/4 flex-none">
+        <div>
+        <h6 class="title is-6">Pane 1a</h6>
+        <p class="subtitle is-6">Fixed width.</p>
+        <p>
+            <small>
+            <strong>Configured with:</strong><br/>
+            minWidth: 100px<br/>
+            width: 150px<br/>
+            maxWidth: 200px<br/>
+            </small>
+        </p>
+        </div>
+    </div>
+    <multipane-resizer ></multipane-resizer>
+    <div class="pane w-1/2 mr-1 flex-none" v-bind:style="[stylePane,stylePaneMid]" >
+        <div>
+        <h6 class="title is-6">Pane 2</h6>
+        <p class="subtitle is-6">Fluid width.</p>
+        <p>
+            <small>
+            <strong>Configured with:</strong><br/>
+            width: 25%<br/>
+            maxWidth: 50%<br/>
+            </small>
+        </p>
+        </div>
+    </div>
+    <multipane-resizer></multipane-resizer>
+    <div class="pane w-1/4 flex-grow" v-bind:style="[stylePane,stylePaneRight]" >
+        <div>
+        <h6 class="title is-6">Pane 3</h6>
+        <p class="subtitle is-6">Takes remaining available space.</p>
 
-    </multipane>
+        <p>
+            <small>
+            <strong>Configured with:</strong><br/>
+            flex-grow: 1<br/>
+            </small>
+        </p>
+        </div>
+    </div>
+    
+    </div>
+</multipane>
   `,
   data: function () {
       return {
           nextElementSiblingStart: null,
           resizeElementChildWidthStart: null,
           resizeElementChildSizeStart: null,
-          moved: false
+          resizeElementChildSizeMax: 0,
+          styleObject1: {
+            width: '100%',
+            height: '400px',
+            border: '1px solid #ccc',
+                'align-content': 'stretch'
+            },
+            stylePane: {
+                'text-align': 'left',
+                overflow: 'hidden',
+                background: '#eee',
+                'border-left': '1px solid #ccc'
+            },
+            stylePaneMid: {
+                minWidth: '10%'
+            },
+            stylePaneLeft: {
+                minWidth: '10%'
+            },
+            stylePaneRight: {
+                minWidth: '10%'
+            },
       }
   },
   mounted: function () {
@@ -28,12 +93,10 @@ export default Vue.component('test', {
     paneResizeStart: function (pane, resizer, size) { 
         if(pane.nextElementSibling){
             if(pane.nextElementSibling.nextElementSibling){
-                //pane.style.width=(parseInt(pane.style.width, 10)+1)+"px";
-                this.nextElementSiblingStart=pane.nextElementSibling.nextElementSibling.clientWidth;
-                this.resizeElementChildWidthStart=pane.clientWidth;
-                this.resize(pane, resizer, size);
-                //pane.style.width=this.resizeElementChildWidthStart;
-                this.moved = false;
+                var currentSize = parseInt(size, 10);
+                this.resizeElementChildSizeMax=currentSize;
+                this.nextElementSiblingStart=pane.nextElementSibling.nextElementSibling.offsetWidth;
+                this.resizeElementChildWidthStart=pane.offsetWidth;
             }
         }
     },
@@ -45,6 +108,11 @@ export default Vue.component('test', {
         }
     },
     paneResizeStop: function (pane, resizer, size) {
+        if(pane.nextElementSibling){
+            if(pane.nextElementSibling.nextElementSibling){
+                this.resize(pane, resizer, size);
+            }
+        }
     },
     resize: function (pane, resizer, size) {
         var next = pane.nextElementSibling.nextElementSibling;
@@ -53,15 +121,17 @@ export default Vue.component('test', {
         var currentMin = parseInt(pane.style.minWidth, 10);
         var offset = this.resizeElementChildWidthStart-currentSize;
         var nextElementSiblingEnd=this.nextElementSiblingStart+offset;
-        if( nextElementSiblingEnd > nextMin && currentSize > currentMin && offset != 0 && this.moved){
+        if( nextElementSiblingEnd > nextMin && currentSize > currentMin && offset != 0 ){
             next.style.width=nextElementSiblingEnd+ "px";
-            var resizeElementChildWidthMax=size;
-            pane.style.width=resizeElementChildWidthMax;
+            this.resizeElementChildSizeMax=currentSize;
         }
-        this.moved = true;
+        else{
+            pane.style.width=this.resizeElementChildSizeMax+"px";
+        }
     }
   },
   components: {
-    Multipane
+    Multipane,
+    MultipaneResizer
   }
 });
--- app/components/vue-multipaneresizer.mjs
+++
@@ -1,6 +0,0 @@
-var LAYOUT_HORIZONTAL = 'horizontal';
-var LAYOUT_VERTICAL = 'vertical';
-export default Vue.component('multipane-resizer', {
-    render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"multipane-resizer"},[_vm._t("default")],2)},
-    staticRenderFns: [],
-});
